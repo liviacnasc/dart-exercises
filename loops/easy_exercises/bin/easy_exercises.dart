9624c807050a29e9780d2b51e5bfbb324ced7b17
liviacnasc <liviac.nasc@gmail.com>
2024-05-27 09:10:42 -0300

    add: loops easy exercises

---
+++ loops/easy_exercises/bin/easy_exercises.dart
@@ -0,0 +1,19 @@
+import 'package:easy_exercises/easy_exercises.dart' as easy_exercises;
+
+void main(List<String> arguments) {
+  print("---- 01 ----");
+
+  easy_exercises.visitorCount();
+
+  print("---- 02 ----");
+
+  easy_exercises.productCount();
+
+  print("---- 03 ----");
+
+  easy_exercises.countdown();
+
+  print("---- 04 ----");
+
+  easy_exercises.squareCount();
+}
---
+++ loops/easy_exercises/lib/easy_exercises.dart
@@ -0,0 +1,25 @@
+void visitorCount(){
+  for(int i = 0; i < 10; i++){
+    print(i);
+  }
+}
+
+void productCount(){
+  for(int i = 0; i < 10; i++){
+    if(i % 2 == 0){
+      print(i);
+    }
+  }
+}
+
+void countdown(){
+  for(int i = 9; i >= 0; i--){
+    print(i);
+  }
+}
+
+void squareCount(){
+  for(int i = 1; i < 11; i++){
+    print(i * i);
+  }
+}
